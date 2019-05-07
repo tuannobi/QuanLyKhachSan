3d887fab34845c494d33e34dc5a671b40cba8253
17521284 <50091679+17521284@users.noreply.github.com>
2019-05-07 16:47:24 +0700

    a

--- src/controller/QuanLyKhachHangController.java
+++ src/controller/QuanLyKhachHangController.java
@@ -5,14 +5,149 @@
  */
 package controller;
 
-import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Vector;
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import view.QuanLyKhachHangJPanel;
-import view.QuanLyPhongJPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import miniPopup.SuaKhachHangJDialog;
+import model.KhachHang;
+import service.KhachHangService;
 
 public class QuanLyKhachHangController {
    
+    private JTextField jtfTimKiem;
+    private JButton jbtCapNhat,jbtXoa;
+    private DefaultTableModel dtmKhachHang;
+    ArrayList<KhachHang> listKH;
+    KhachHang khachhang;
+    KhachHangService khService;
+    private JTable tbKhachHang;
+    
+        public QuanLyKhachHangController(JTextField jtfTimKiem, JButton jbtCapNhat, JButton jbtXoa,DefaultTableModel dtmKhachHang, ArrayList<KhachHang> listKH,JTable tbKhachHang) {
+        this.jtfTimKiem = jtfTimKiem;
+        this.jbtCapNhat = jbtCapNhat;
+        this.jbtXoa = jbtXoa;
+        this.dtmKhachHang = dtmKhachHang;
+        this.listKH=listKH;
+        this.tbKhachHang=tbKhachHang;
+        
+        addEvents();
+    }
+        
+        public void addEvents(){
+        tbKhachHang.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int row=tbKhachHang.getSelectedRow();
+                if (row==-1)
+                    return;
+                khachhang=listKH.get(row); //lấy giá trị kh khi click chuột vào
+                
+               // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+              //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+              //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+              //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+               // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+//        jbtCapNhat.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                if (khachhang==null)
+//                    JOptionPane.showMessageDialog(null, "Vui lòng chọn khách hàng bạn muốn chỉnh sửa");
+//                else
+//                {
+//                    suaKhachHangJDialog=new SuaKhachHangJDialog();
+//                    suaKhachHangJDialog.showWindows();
+//                    suaKhachHangJDialog.setTextHoTen(khachhang.getHoTen());
+//                    suaKhachHangJDialog.setTextMaKH(khachhang.getMaKH());
+//                    /*
+//                    chuyển Date sang string
+//                    */
+//                    SimpleDateFormat formater=new SimpleDateFormat("dd/MM/yyyy");                 
+//                    suaKhachHangJDialog.setTextNgaySinh(formater.format(khachhang.getNgaySinh()));
+//                    suaKhachHangJDialog.setTextCMND( String.valueOf(khachhang.getCMND()));
+//                    suaKhachHangJDialog.setTextGioiTinh(khachhang.getGioiTinh());
+//                    
+//                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//            }
+//            }
+//        });
+        
+        jbtXoa.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (khachhang==null)
+                {
+                    JOptionPane.showMessageDialog(null, "Vui lòng chọn khách hàng muốn xóa!");
+                }
+                else
+                {
+                    int result=JOptionPane.showConfirmDialog(null, "Bạn có muốn xóa khách hàng này không?","Warning", JOptionPane.YES_NO_OPTION);
+                    if (result==JOptionPane.YES_OPTION){
+                        int count=khService.xoaDuLieu(khachhang.getMaKH());
+                        if (count!=-1){
+                        JOptionPane.showMessageDialog(null, "Đã xóa thành công "+count+" khách hàng");
+                        refreshData();
+                        }
+                        else
+                            JOptionPane.showMessageDialog(null, "Không tìm thấy khách hàng cần xóa");
+                    }
+                }
+               // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+    }
+   
+
+   public void hienThiDuLieuKhachHang(){
+       // Giúp cập nhật bảng sau mỗi thao tác
+      
+       dtmKhachHang=(DefaultTableModel)tbKhachHang.getModel();
+       khService=new KhachHangService();
+       listKH=new ArrayList<KhachHang>();
+       listKH=khService.getDuLieuKhachHang();
+       for (KhachHang kh : listKH){
+           Vector<Object> vec=new Vector<Object>();
+           vec.add(kh.getMaKH());
+           vec.add(kh.getHoTen());
+           vec.add(kh.getNgaySinh());
+           vec.add(kh.getCMND());
+           vec.add(kh.getGioiTinh());
+           dtmKhachHang.addRow(vec);
+           tbKhachHang.setModel(dtmKhachHang);
+       }
+   }
+   
+   private void refreshData(){
+       dtmKhachHang.setRowCount(0);
+       hienThiDuLieuKhachHang();
+       
+   }
+    
 }
--- src/model/DichVu.java
+++ src/model/DichVu.java
@@ -10,5 +10,31 @@ package model;
  * @author Tuan
  */
 public class DichVu {
-    
+    private int maDichVu;
+    private String tenDichVu;
+    private float giaTien;
+
+    public int getMaDichVu() {
+        return maDichVu;
+    }
+
+    public String getTenDichVu() {
+        return tenDichVu;
+    }
+
+    public float getGiaTien() {
+        return giaTien;
+    }
+
+    public void setMaDichVu(int maDichVu) {
+        this.maDichVu = maDichVu;
+    }
+
+    public void setTenDichVu(String tenDichVu) {
+        this.tenDichVu = tenDichVu;
+    }
+
+    public void setGiaTien(float giaTien) {
+        this.giaTien = giaTien;
+    }  
 }
